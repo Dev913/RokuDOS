6f819b038b8425c7865f39ae73f81903e2c261df
Parker Charles Phillips <58945995+ContactDev913@users.noreply.github.com>
2021-01-15 18:26:32 +0100

    Add files via upload

--- RokuDOS.py
+++ RokuDOS.py
@@ -1,18 +1,12 @@
 from os import system
 import time
-import yaml
 system('pip install roku')
-system('reset')
-print('Importing roku module for python! (in 1 seconds)')
-time.sleep(1)
-system('reset')
+system('clear')
 from roku import Roku
-ip = raw_input('Please input the IP of the roku: ')
+system('clear')
+ip = input('Please input the IP of the roku: ')
 roku = Roku('' + ip)
-with open('Data/data.yaml', 'r') as data:
-    data = yaml.load(data)
-user_preference = data['delay']
 while time.time:
     roku.home()
-    print('Done...')
-    time.sleep(user_preference)
+    print('Done.')
+    time.sleep(1)
