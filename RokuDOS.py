5f5fbb366164c1ed6f7dd2f81c571a71e6cfb9bf
Parker Charles Phillips <58945995+ContactDev913@users.noreply.github.com>
2021-01-17 14:29:08 +0100

    Add files via upload

--- RokuDOS.py
+++ RokuDOS.py
@@ -1,6 +1,7 @@
 from os import system
 import time
-system('pip install roku')
+system('sudo apt-get install python3-pip')
+system('pip3 install roku')
 system('clear')
 from roku import Roku
 system('clear')
